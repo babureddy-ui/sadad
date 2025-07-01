34874662d64e29c4407cf443dba7b7d0b9080e77
babureddy-ui <babureddy@queuebuster.co>
2025-07-01 11:32:27 +0530

    WhatsApp Channel Link has been aded in footer

--- src/components/footerComponents/DesktopFooter.jsx
+++ src/components/footerComponents/DesktopFooter.jsx
@@ -112,11 +112,11 @@ const DesktopFooter = () => {
           </a>
 
           <p className={styles.footerContent}>Call : 02013444300</p>
-          <a href="https://x.com/doroki_ng" target="" rel="noopener noreferrer"> 
-          <p className={styles.footerContent}>X : @doroki_ng</p>
+          <a href="https://x.com/_dorokibypaga" target="" rel="noopener noreferrer"> 
+          <p className={styles.footerContent}>X : @_dorokibypaga</p>
           </a>
-          <p className={styles.footerContent}>Facebook : @mypaga</p>
-          <p className={styles.footerContent}>WhatsApp : 08099227242</p>
+          <a href="https://web.facebook.com/people/Doroki-by-Paga/61576983554167/"><p className={styles.footerContent}>Facebook : @Doroki-by-Paga</p> </a>
+           <a href="https://wa.me/2348099227242" target='_blank' rel="noopener noreferrer">  <p className={styles.footerContent}>WhatsApp : 08099227242</p></a>
           <p className={styles.footerContent}>SMS : 09090027242</p>
           <p className={styles.footerContent}>Address : 176 Herbert <br />
            Macaulay Wy, Yaba, Lagos</p>
